2a91d67fc3acf6417ad0d117d688a012fd8bb798
jessie <@blanc-AL003>
2022-08-24 20:35:16 +0800

    add and update ———— 2022年 08月 24日 星期三 20:35:16 CST

--- Java/IO_Utils/src/IO_Utils.java
+++ Java/IO_Utils/src/IO_Utils.java
@@ -1,6 +1,8 @@
 import javax.imageio.metadata.IIOMetadata;
 import javax.swing.*;
 import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -18,6 +20,7 @@ public class IO_Utils {
 
     /**
      * 读取文件，以行位单位返回列表
+     * 仅适用于单行少的
      * @param f 文件类
      * @return 每一行的列表
      */
@@ -59,52 +62,9 @@ public class IO_Utils {
     }
 
 
-    public static byte[] readBytesUsingBufferedInputStream(File f){
-        Path path = Paths.get(f.getAbsolutePath());
-        try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(path.toFile()));
-             ByteArrayOutputStream baos = new ByteArrayOutputStream();){
-            byte[] read = new byte[4];
-            int r_len = -1;
-            while ((r_len = bis.read(read)) != -1){
-                baos.write(read, 0, r_len);
-            }
-            baos.flush();
-            return baos.toByteArray();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-
-    public static void writeBytesUsingBufferedOutputStream(File f, byte[] data){
-        Path path = Paths.get(f.getAbsolutePath());
-        try (BufferedInputStream bis = new BufferedInputStream(new ByteArrayInputStream(data));
-             BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(path.toFile()));){
-            int read = -1;
-            byte[] buf = new byte[1024];
-            while ((read = bis.read(buf)) != -1){
-                bos.write(buf, 0, read);
-            }
-            bos.flush();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-
-    public static byte[] readBytesUsingFileInputStream(File f){
-        try{
-            return new FileInputStream(f).readAllBytes();
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
     /**
-     * 使用Scanner读取较大的文件
+     * 使用Scanner读取较大的文件，但是花时也长
      * 更多请阅读：https://www.yiibai.com/java/java-read-text-file.html
-     * 超大文件TODO https://blog.51cto.com/u_9597987/3485702
      * @param f 文件类
      * @return LinkedList<String>
      */
@@ -123,6 +83,102 @@ public class IO_Utils {
         }catch (Exception e) {
             e.printStackTrace();
             return null;
+        }
+    }
+
+
+    /**
+     * 读取小的文本文件
+     * 本质上使用BufferedReader
+     * @param f
+     * @return
+     */
+    public static LinkedList<String> readUsingFiles(File f){
+        try{
+            return (LinkedList<String>) Files.readAllLines(Paths.get(f.getAbsolutePath()));
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    /**
+     * 读取二进制文件
+     * @param f
+     * @return
+     */
+    public static byte[] readBytesUsingBufferedInputStream(File f){
+        Path path = Paths.get(f.getAbsolutePath());
+        try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(path.toFile()));
+             ByteArrayOutputStream baos = new ByteArrayOutputStream();){
+            byte[] read = new byte[4];
+            int r_len = -1;
+            while ((r_len = bis.read(read)) != -1){
+                baos.write(read, 0, r_len);
+            }
+            baos.flush();
+            return baos.toByteArray();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    /**
+     * 写入二进制文件
+     * @param f
+     * @param data
+     */
+    public static void writeBytesUsingBufferedOutputStream(File f, byte[] data){
+        Path path = Paths.get(f.getAbsolutePath());
+        try (BufferedInputStream bis = new BufferedInputStream(new ByteArrayInputStream(data));
+             BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(path.toFile()));){
+            int read = -1;
+            byte[] buf = new byte[1024];
+            while ((read = bis.read(buf)) != -1){
+                bos.write(buf, 0, read);
+            }
+            bos.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    /**
+     * 读取小的二进制文件
+     * @param f
+     * @return
+     */
+    public static byte[] readBytesUsingFileInputStream(File f){
+        try{
+            return new FileInputStream(f).readAllBytes();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void readUsingFileFileChannel(File f) {
+        try (FileInputStream fileInputStream = new FileInputStream(f);) {
+            FileChannel fileChannel = fileInputStream.getChannel();
+            int capacity = 1024 * 1024;//1M (1 * 1024 * 1024)
+            ByteBuffer byteBuffer = ByteBuffer.allocate(capacity);
+            StringBuilder buffer = new StringBuilder();
+            while( fileChannel.read(byteBuffer) != -1) {
+                //读取后，将位置置为0，将limit置为容量, 以备下次读入到字节缓冲中，从0开始存储
+                byteBuffer.clear();
+                byte[] bytes = byteBuffer.array();
+                String str = new String(bytes);
+                //System.out.println(str);
+                //处理字符串,并不会将字符串保存真正保存到内存中
+                // 这里简单模拟下处理操作.
+                buffer.append(str.substring(0,1));
+            }
+            System.out.println("buffer.length:"+buffer.length());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
