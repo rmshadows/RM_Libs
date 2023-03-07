8218c9aef630af0021af992d23efe69fd6630230
ryan <ryan@>
2023-03-08 05:35:31 +0800

    update —— 2023年 03月 08日 星期三 05:35:31 CST

--- Java/Datetime_Utils/src/Datetime_Utils/Datetime_Utils.java
+++ Java/Datetime_Utils/src/Datetime_Utils/Datetime_Utils.java
@@ -3,6 +3,8 @@ package Datetime_Utils;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.LinkedList;
+import java.util.Objects;
+import java.util.concurrent.atomic.DoubleAdder;
 
 /**
  * 时间日期工具类
@@ -53,6 +55,23 @@ public class Datetime_Utils {
 		}
 	}
 
+	/**
+	 * 返回d1， d2时间差数据
+	 * @param d1
+	 * @param d2
+	 * @return 列表：1.int数组年月日 2.字符串时间差
+	 */
+	public static LinkedList<Object> periodLocalDate(LocalDate d1, LocalDate d2){
+		LinkedList<Object> ll = new LinkedList<>();
+		Period p = Period.between(d1, d2);
+		//
+		ll.add(new int[]{p.getYears(), p.getMonths(), p.getDays()});
+		// 时间差字符串
+		String s = "目标日期 "+ d1 +" 距离 "+ d2 +" 的时间差："+p.getYears()+" 年 "+p.getMonths()+" 月 "+p.getDays()+" 天";
+		ll.add(s);
+		return ll;
+	}
+
 	// 日期时间
 	/**
 	 * 返回当前日期时间
@@ -97,8 +116,46 @@ public class Datetime_Utils {
 		return ll;
 	}
 
+	/**
+	 * 返回d1， d2时间差数据
+	 * @param d1
+	 * @param d2
+	 * @return 列表：1.long数组 天、时、分、秒、毫秒、微秒 2.字符串时间差
+	 */
+	public static LinkedList<Object> durationLocalDate(LocalDateTime d1, LocalDateTime d2){
+		LinkedList<Object> ll = new LinkedList<>();
+		Duration d = Duration.between(d1, d2);
+		// 天、时、分、秒、毫秒、微秒
+		ll.add(new long[]{d.toDays(), d.toHours(), d.toMinutes(), d.toSeconds(), d.toMillis(), d.toNanos()});
+		// 时间差字符串
+		String s = d1 +" 和 "+ d2 +" 的时间差： 天数："+d.toDays()+
+				" 小时数："+ d.toHours()+
+				" 分钟数："+d.toMinutes()+
+				" 秒数："+d.toSeconds()+
+				" 毫秒数："+d.toMillis()+
+				" 微秒数："+d.toNanos();
+		ll.add(s);
+		System.out.println(s);
+		return ll;
+	}
+
 	// 格式
 	// ISO 8601规定的日期和时间分隔符是T。标准格式如下：
+	// yyyy	4位数的年份
+	//yy	显示2位数的年份，比如2022年，则显示为22年
+	//MM	显示2位数的月份，不满2位数的，前面补0，比如7月份显示07月
+	//M	月份，不满2位的月份不会补0
+	//dd	天， 如果1位数的天数，则补0
+	//d	天，不满2位数字的，不补0
+	//HH	24小时制的时间显示，小时数，两位数，不满2位数字的前面补0
+	//H	24小时制的时间显示，小时数，不满2位数字的不补0
+	//hh	12小时制的时间显示，小时数，两位数，不满2位数字的前面补0
+	//ss	秒数，不满2位的前面补0
+	//s	秒数，不满2位的不补0
+	//SSS	毫秒数
+	//z	时区名称，比如北京时间东八区，则显示CST
+	//Z	时区偏移信息，比如北京时间东八区，则显示+0800
+
 	/**
 	 * 获取格式
 	 * @param format 如"yyyy-MM-dd"
@@ -120,8 +177,11 @@ public class Datetime_Utils {
 		return Clock.system(ZoneId.of(timezone));
 	}
 
+	/**
+	 * 获取系统默认时区
+	 * @return 时区
+	 */
 	public static ZoneId getSystemTimezone() {
 		return ZoneId.systemDefault();
 	}
-
 }
--- Java/Datetime_Utils/src/Datetime_Utils/Test.java
+++ Java/Datetime_Utils/src/Datetime_Utils/Test.java
@@ -1,9 +1,7 @@
 package Datetime_Utils;
 
 import javax.swing.*;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.LinkedList;
@@ -11,7 +9,14 @@ import java.util.LinkedList;
 public class Test {
 	public static void main(String[] args) {
 //		System.out.println(Datetime_Utils.getTimeNow(null));
-		LocalDateTime lt = Datetime_Utils.getDateTimeNow(null);
-		System.out.println(lt);
+		LocalDateTime ltd1 = LocalDateTime.of(2023,1,2,3,4);
+		LocalDateTime ltd2 = LocalDateTime.of(2023,1,2,3,5);
+		System.out.println(Datetime_Utils.durationLocalDate(ltd1, ltd2));
+		System.out.println();
+
+//		LocalDateTime lt = Datetime_Utils.getDateTimeNow(null);
+//		System.out.println(lt);
+//		Datetime_Utils.getFirstAndLastDayofmonth(null);
+//		System.out.println(lt);
 	}
 }
