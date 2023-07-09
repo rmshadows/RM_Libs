69996a2c00fd1c20b6d3772b13c19ca0b4ead16c
jessie <justaaaa@163.com>
2023-07-10 00:27:46 +0800

    add and update ———— 2023年 07月 10日 星期一 00:27:46 CST

--- Nodejs/msystem/msystem.mjs
+++ Nodejs/msystem/msystem.mjs
@@ -1,6 +1,50 @@
+"use strict";
 import clipboard from 'clipboardy';
 import { log } from 'console';
+import { randomFill } from 'crypto';
+import { resolve } from 'dns';
 import fs from 'fs';
+import { endianness } from 'os';
+var fsPromise = fs.promises;
+
+/**
+ * 模块内部函数
+ */
+/**
+ * 获取当前运行的函数名称
+ * @returns type:function 函数名
+ */
+export const getExecFunction = () => {
+    let names = new Error().stack.match(/at (.*?) /g);
+    let name = names[1].replace('at ', '').trim();
+    return eval(name);
+}
+/**
+ * 默认回调函数
+ * let name = getExecFunction().name;
+ * fs.mkdir(dir, { recursive: recursive, mode: mode }, (err, data) => defaultCallback(err, data, name));
+ * @param {*} err 
+ * @param {*} data 
+ * @param {string} funcname 函数名 
+ */
+const defaultCallback = (err, data, funcname) => {
+    if (err) {
+        console.log(err);
+    } else {
+        // process.stdout.write(funcname);
+        console.log("Default callback: Function " + funcname + "() has reached callback.");
+    }
+}
+/**
+ * 默认的用于Promise的函数
+ * @param {*} fn 参数列表 这里默认函数名
+ */
+const onSuccess = (fn) => {
+    console.log(fn[0] + " Success.");
+}
+const onFailed = (fn) => {
+    console.log(fn[0] + " Failed.");
+}
 
 
 /**
@@ -8,11 +52,11 @@ import fs from 'fs';
  */
 /**
  * clipboardy设置粘贴板
- * @param {*} content content 内容
- * @param {*} sync 是否同步，Doesn't work in browsers.
- * @returns sync:返回字符串内容 sync false:返回Promise
+ * @param {string} content content 内容
+ * @param {boolean} sync 是否同步， if true Doesn't work in browsers.
+ * @returns 返回字符串内容
  */
-export const setClipboard = (content, sync = true) => {
+export const setClipboard = async (content, sync = true) => {
     let result = false;
     if (sync) {
         // Doesn't work in browsers.
@@ -28,27 +72,27 @@ export const setClipboard = (content, sync = true) => {
             console.log("setClipboard failed:" + error);
         }
     } else {
-        try {
-            clipboard.write(content);
-            if (clipboard.read() === content) {
+        clipboard.write(content).then(async () => {
+            if (await clipboard.read() === content) {
                 result = true;
             } else {
                 console.log("setClipboard: 似乎失败了");
             }
             console.log("setClipboard:设置粘贴板:" + content);
-        } catch (error) {
+        }).catch((err) => {
             console.log("setClipboard failed:" + error);
-        }
+        });
     }
     return result;
 };
 
+
 /**
  * clipboardy获取系统粘贴板信息
- * @param {*} sync 是否同步，Doesn't work in browsers.
- * @returns sync:返回字符串内容 sync false:返回Promise
+ * @param {boolean} sync 是否同步，If false, Doesn't work in browsers.
+ * @returns 返回字符串内容
  */
-export const getClipboard = (sync = true) => {
+export const getClipboard = async (sync = true) => {
     let content = "";
     if (sync) {
         try {
@@ -58,48 +102,95 @@ export const getClipboard = (sync = true) => {
             console.log("getClipboard failed:" + error);
         }
     } else {
-        try {
-            content = clipboard.read();
-            console.log("getClipboard:粘贴板读取:" + content);
-        } catch (error) {
-            console.log("getClipboard failed:" + error);
-        }
+        clipboard.read().then((data) => {
+            console.log("getClipboard:粘贴板读取:" + (content = data));
+        }).catch((err) => {
+            console.log("getClipboard(): " + err);
+        });
     }
     return content;
 }
 
+
 /**
  * 文件IO操作
  */
-// TODO
 
+export const fileType = async (filepath, sync = true) => {
+    let fn = getExecFunction().name;
+    let t = 0;
+    try {
+        if (sync) {
+            let fstat = fs.lstatSync(filepath);
+            if (fstat.isSymbolicLink()) {
+                console.log(filepath + "是链接文件");
+                return 3;
+            } else if (fstat.isFile()) {
+                console.log(filepath + "是文件");
+                return 1;
+            } else if (fstat.isDirectory()) {
+                console.log(filepath + "是文件夹");
+                return 2;
+            } else {
+                console.log(filepath + "处于未知状态");
+                return -1;
+            }
+        } else {
+            t = await fsPromise.lstat(filepath).then((fstat) => {
+                if (fstat.isSymbolicLink()) {
+                    console.log(filepath + "是链接文件");
+                    return 3;
+                } else if (fstat.isFile()) {
+                    console.log(filepath + "是文件");
+                    return 1;
+                } else if (fstat.isDirectory()) {
+                    console.log(filepath + "是文件夹");
+                    return 2;
+                } else {
+                    console.log(filepath + "处于未知状态");
+                    return -1;
+                }
+            });
+        }
+        return t;
+    } catch (error) {
+        console.log("fileType(): " + error);
+    }
+}
+
+var a = fileType("3", true)
+console.log(typeof (a));
+console.log(a);
 
 /**
  * 新建文件夹
- * @param {*} dir 路径
- * @param {*} sync 是否同步
- * @param {*} overwrite 是否覆盖
- * @param {*} recursive (mkdir -p ?)是否递归
- * @param {*} mode 文件权限默认"0700"
+ * @param {string} dir 路径
+ * @param {boolean} sync 是否同步
+ * @param {boolean} overwrite 是否覆盖
+ * @param {boolean} recursive (mkdir -p ?)是否递归
+ * @param {function list} callback 回调函数 列表传入函数，成功与失败,允许传入一个参数列表
+ * @param {string} mode 文件权限默认"0700"
  * @returns null
  */
-export const mkdir = (dir, sync=true, overwrite = false,recursive = false, mode="0700") => {
+export const mkdir = (dir, sync = true, overwrite = false, recursive = false, callback = [onSuccess, onFailed], mode = "0700") => {
     try {
+        let fn = getExecFunction().name;
         if (fs.existsSync(dir)) {
-            console.log("mkdir:存在同名文件或者文件夹");   
-            if (overwrite){
-                // TODO：删除文件或者文件夹
-            }else{
+            console.log("mkdir:存在同名文件或者文件夹");
+            if (overwrite) {
+                rmDF(dir);
+            } else {
                 return;
             }
         }
-        if(sync){
-            fs.mkdirSync(dir, {recursive: recursive, mode: mode});
-        }else{
-            fs.mkdir(dir, {recursive: recursive, mode: mode}, (err, data)=>{
-                if (err) {
-                    console.log("mkdir:" + err);
-                }
+        if (sync) {
+            fs.mkdirSync(dir, { recursive: recursive, mode: mode });
+        } else {
+            // fs.mkdir(dir, { recursive: recursive, mode: mode }, (err, data) => defaultCallback(err, data, name));
+            fsPromise.mkdir(dir, { recursive: recursive, mode: mode }).then(() => {
+                callback[0]([fn]);
+            }).catch(() => {
+                callback[1]([fn]);
             });
         }
     } catch (error) {
@@ -108,16 +199,31 @@ export const mkdir = (dir, sync=true, overwrite = false,recursive = false, mode=
 }
 
 
+// 删除文件
+export const rmDF = async (filepath, sync = true) => {
+    fn = getExecFunction().name;
+    try {
+        let isFile = fs.stat.isFile(filepath);
+        if (sync) {
+            fs.rmSync(filepath, { recursive: recursive });
+        } else {
+            // fs.rm()
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 
 
 /**
- * 读取文件
+ * 读取文件 TODO
  * @param {*} filepath 文件路径
  * @param {*} readBinary 是否返回二进制内容（否则返回文本） 
  * @param {*} sync 是否同步
  * @returns 内容
  */
-export const readFileContent = (filepath, readBinary = false, sync = true) => {
+export const readFileContent = (filepath, readBinary = false, sync = true, callback = defaultCallback) => {
     let content = "";
     try {
         if (sync) {
@@ -151,7 +257,7 @@ export const readFileContent = (filepath, readBinary = false, sync = true) => {
 
 
 export const mv = (src, dst) => {
-
+    // TODO
     try {
         fs.rename(oldname, newname);
     } catch (error) {
