f93d1d6c143714192a4587474633dede1d0fa18f
ryan <ryan@>
2023-08-11 05:36:25 +0800

    update —— 2023年 08月 11日 星期五 05:36:25 CST

---
+++ Nodejs/maes/maes.mjs
@@ -0,0 +1,13 @@
+var toCrypt = "妳好Hello@";
+var cbfpasswd = "123456"; // 0DF262FE3A6F56282D6E5B9B
+var cfbiv = ";";
+var cbcpasswd = "123456"; // 8494872F6E4EFC08FBD8E3EC57F04A17
+var cbciv = "4321";
+var cbcpwdsize = 32;
+
+import crypto from 'crypto';
+
+
+
+
+
